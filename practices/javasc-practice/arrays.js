928702b3946898d4d0a68b6e1e6d8c559525cb91
Akin-Inceler <akin2001inceler@gmail.com>
2026-04-10 19:28:41 +0200

    Add initial implementation of card and message array manipulations in arrays.js

---
+++ practices/javasc-practice/arrays.js
@@ -0,0 +1,11 @@
+let cards = [7, 3, 9, 5]
+cards.push(2)
+console.log(cards)
+
+
+let messages = ["Hello", "How are you?", "Goodbye"]
+
+let newMessage = "Welcome!"
+messages.push(newMessage)
+messages.pop()
+console.log(messages)
