bd3ab714e8901d7143f3597c014060ae954728ff
Akin-Inceler <akin2001inceler@gmail.com>
2026-04-10 20:16:57 +0200

    Add loop implementations for counting, messages, and card logging in loops.js

---
+++ practices/javasc-practice/loops.js
@@ -0,0 +1,27 @@
+//      start         finish         step
+for ( let count = 1; count <= 10; count +=1) {
+    
+}
+
+
+for (let i =10; i <= 100; i += 10) {
+    
+}
+
+let messages = ["Hello",
+    "How are you?",
+    "Goodbye",
+    "Welcome!",
+    "Have a nice day!",
+    "See you later!"
+]
+for (let n = 0; n < messages.length; n += 1) {
+    
+}
+
+
+
+let cards = [7, 3, 9, 5]
+for (let x = 0; x < cards.length; x += 1) {
+    console.log(cards[x])
+}
