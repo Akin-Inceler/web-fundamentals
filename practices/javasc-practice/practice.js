321653f2b500bea15802c2cfa93949f7666b9192
Akin-Inceler <akin2001inceler@gmail.com>
2026-04-11 17:52:00 +0200

    Add dice rolling functionality and update greeting logging in practice.js

--- practices/javasc-practice/practice.js
+++ practices/javasc-practice/practice.js
@@ -2,4 +2,11 @@ let username = "akin"
 let greeting = "Hi, my name is "
 let myGreeting = greeting + username 
 
-console.log(myGreeting)
+//console.log(myGreeting)
+let randomNumber =Math.floor(Math.random() *6) + 1
+
+function rollDice() {
+    let randomNumber =Math.floor(Math.random() *6) + 1
+    return randomNumber
+}
+console.log(rollDice())
