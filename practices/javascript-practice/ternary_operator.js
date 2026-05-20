a917408311e2b9e54a7a3027d2d67e09dd9c8efc
Akın İnceler <akin2001inceler@gmail.com>
2026-05-20 21:57:42 +0200

    Create ternary_operator.js

---
+++ practices/javascript-practice/ternary_operator.js
@@ -0,0 +1,19 @@
+// let message = ''
+
+// if (exerciseTimeMins < 30) {
+//   message = 'You need to try harder!'
+// } else if (exerciseTimeMins < 60) {
+//   message = 'Doing good!'
+// } else {
+//   message = 'Excellent!'
+// }
+
+const exerciseTimeMins = 45
+
+const message = exerciseTimeMins < 30 
+  ? 'You need to try harder!' 
+  : exerciseTimeMins < 60 
+    ? 'Doing good!' 
+    : 'Excellent!'
+
+console.log(message)
