195f8c074e58137124b5959c42a2b342d7b5a24d
Akın İnceler <akin2001inceler@gmail.com>
2026-06-01 17:07:59 +0200

    Remove async function for fetching activity data from API

--- practices/javascript-practice/fetching.js
+++
@@ -1,8 +0,0 @@
-async function getActivity() {
-  const response = await fetch("https://apis.scrimba.com/bored/api/activity")
-  const data = await response.json()
-
-  console.log(data)
-}
-
-getActivity()
