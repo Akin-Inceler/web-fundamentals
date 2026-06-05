58c8bd36cc050a38b6b47a4ebf0b0c7c4f7a9407
Akın İnceler <akin2001inceler@gmail.com>
2026-06-05 17:23:03 +0200

    Refactor project structure: update package.json to use module type and remove project_details.md; implement basic server in server.js

--- Node.js/server.js
+++ Node.js/server.js
@@ -1,1 +1,11 @@
-console.log("We have a package.json!!!")
+import http from 'node:http'
+
+const PORT = 8000
+
+const server = http.createServer((req, res) => {
+    res.end('Hello World!')
+})
+
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
