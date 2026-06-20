845620e7d7e95e5105ca351f4925c3656217c917
Akın İnceler <akin2001inceler@gmail.com>
2026-06-21 01:13:35 +0300

    Enhance server response: add additional data to response in server.js

--- Node.js/server.js
+++ Node.js/server.js
@@ -3,7 +3,11 @@ import http from 'node:http'
 const PORT = 8000
 
 const server = http.createServer((req, res) =>{
-    res.end('This is from the server!!')
+    res.write('This is a some data \n')
+    res.write('This is a some more data \n')
+    res.end('This is from the server!!', 'utf-8', () => {
+        console.log('Response sent successfully!')
+    })
 })
 
 server.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
