2b0c5a31057a3d00630549aeb4da7e1b3d89740a
Akın İnceler <akin2001inceler@gmail.com>
2026-06-22 11:46:19 +0300

    Implement data retrieval and server response: add db.js and data.js, update server.js to serve data from API

---
+++ Node.js/data.js
@@ -0,0 +1,32 @@
+export const data = [
+    {
+    name: 'John Doe',
+    location: 'New York',
+    country: 'USA',
+    continent: 'North America',
+    is_open_to_public: true,
+    uuid: '123e4567-e89b-12d3-a456-426614174000',
+    details: [{
+        description: 'A beautiful park in the heart of the city.',
+        established: 1857,
+        area: '843 acres',
+        attractions: ['Zoo', 'Botanical Garden', 'Lake']
+    }
+    ]
+    },
+    {
+    name: 'Eiffel Tower',
+    location: 'Paris',
+    country: 'France',
+    continent: 'Europe',
+    is_open_to_public: true,
+    uuid: '123e4567-e89b-12d3-a456-426614174001',
+    details: [{
+        description: 'An iconic iron lattice tower in Paris.',
+        established: 1889,
+        height: '324 meters',
+        attractions: ['Observation Deck', 'Restaurants', 'Light Show']
+    }
+    ]
+    }
+]
--- Node.js/server.js
+++ Node.js/server.js
@@ -1,13 +1,14 @@
 import http from 'node:http'
+import { getDataFromDB } from './db.js'
 
 const PORT = 8000
 
-const server = http.createServer((req, res) =>{
-    res.write('This is a some data \n')
-    res.write('This is a some more data \n')
-    res.end('This is from the server!!', 'utf-8', () => {
-        console.log('Response sent successfully!')
-    })
+const server = http.createServer(async (req, res) => {
+    const destinations = await getDataFromDB()
+
+    if (req.url === '/api' && req.method === 'GET') {
+        res.end(JSON.stringify(destinations, null, 2))
+    }
 })
 
 server.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
