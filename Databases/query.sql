7e692dc196f44145ef7fe129161c764ffa0e6c7a
Akın İnceler <akin2001inceler@gmail.com>
2026-06-29 01:27:03 +0300

    Refactor SQL queries for car selection by adding additional filters and conditions

--- Databases/query.sql
+++ Databases/query.sql
@@ -5,3 +5,28 @@ WHERE color = 'red';
 
 SELECT * FROM cars 
 WHERE price < 1250;
+
+
+SELECT * FROM cars 
+WHERE year != 1999;
+
+
+SELECT * FROM cars 
+WHERE color != 'red';
+
+
+SELECT * FROM cars 
+WHERE color LIKE '%red%';
+
+
+SELECT * FROM cars 
+WHERE color NOT LIKE '%red%';
+
+
+SELECT * FROM cars 
+WHERE model LIKE 's_';
+
+
+SELECT * FROM cars 
+WHERE color NOT LIKE '%black%'
+AND model LIKE 's_';
