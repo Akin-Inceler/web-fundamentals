a88df86887543b7965906e235ffbee30977853bd
Akın İnceler <akin2001inceler@gmail.com>
2026-07-06 13:46:47 +0300

    Update SQL queries to correct value thresholds, refine selection criteria, and improve ordering

--- Databases/query.sql
+++ Databases/query.sql
@@ -1,14 +1,16 @@
 SELECT * FROM cars;
 
+
 SELECT brand, model, color, year, value FROM cars 
 WHERE color = 'red';
 
+
 SELECT * FROM cars 
-WHERE value < 1250;
+WHERE value < 11250;
 
 
 SELECT * FROM cars 
-WHERE year != 1999;
+WHERE year != 2020;
 
 
 SELECT * FROM cars 
@@ -24,23 +26,45 @@ WHERE color NOT LIKE '%red%';
 
 
 SELECT * FROM cars 
-WHERE model LIKE 's_';
+WHERE model LIKE 'X_';
 
 
 SELECT * FROM cars 
-WHERE color NOT LIKE '%black%'
-AND model LIKE 's_';
+WHERE color NOT LIKE '%gray%'
+AND model LIKE 'X_';
 
 
 SELECT brand, model, color FROM cars 
-WHERE color NOT IN ('red', 'blue')
-AND brand NOT IN ('Toyota', 'Honda', 'volco');
+WHERE color NOT IN ('black', 'red', 'blue')
+AND brand NOT IN ('Toyota', 'Honda', 'volvo');
 
 
 SELECT * FROM cars 
 WHERE ((brand = 'bmw' AND color = 'red') 
-OR (brand IN ('Honda', 'volvo') AND year BETWEEN 2000 AND 2010));
+OR (brand IN ('Honda', 'Volvo') AND year BETWEEN 2020 AND 2025))
+AND SOLD IS NOT TRUE;
 
 
 SELECT brand, model, year FROM cars
  ORDER BY brand;
+
+
+SELECT brand, model, year FROM cars
+ORDER BY brand DESC, year ASC;
+
+
+SELECT brand, model, year, sold FROM cars
+WHERE SOLD IS FALSE
+ORDER BY brand DESC, year ASC;
+
+
+SELECT brand, model, year, value FROM cars
+ ORDER BY value DESC
+ LIMIT 1;
+
+
+SELECT brand, model, year, color, value FROM cars
+WHERE color LIKE '%red%'
+AND sold IS FALSE
+ORDER BY value DESC
+LIMIT 5;
